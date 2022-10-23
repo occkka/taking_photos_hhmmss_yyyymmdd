1f45ee73495975c1d17f17c741d61c40f2edf22f
occkka <47744839+occkka@users.noreply.github.com>
2022-10-23 17:20:50 +0900

    Update 24h_images.py

--- 24h_images.py
+++ 24h_images.py
@@ -7,6 +7,10 @@ def hconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
                       for im in im_list]
     return cv2.hconcat(im_list_resize)
 
+output_dir = "24h_images"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
 for i in range(0, 24):
     tgt_h = str(i).zfill(2)
     files = glob.glob("all_photos/" + tgt_h + "*")
@@ -22,7 +26,6 @@ for i in range(0, 24):
     else:
         #Arrange the photos in order horizontally / 写真を横に並べる
         im_h_resize = hconcat_resize_min(images)
-        #
         #Set the size of the output image / 出力画像のサイズを決める
         img_resize = cv2.resize(im_h_resize, dsize=(4618, 3464))
         cv2.imwrite('24h_images/' + tgt_h + '.jpg', img_resize)
